fc7ba957c6efd7faf67be3debda7f8a6d36a7dc6
Javan Kyalo <javankyalo2@gmail.com>
2022-07-15 12:09:56 +0300

    data export

---
+++ backend/database/data/endpoints_202207151206.sql
@@ -0,0 +1,2 @@
+INSERT INTO ensemble.dbo.endpoints (endpointId,name,description,project,url) VALUES
+	 (N'b5587458-4eca-44c7-adfb-174ecf32938c',N'get users',N'get all users',N'1d277069-a873-4d28-a527-eace8e286bd1',N'/users/get');
---
+++ backend/database/data/roles_202207151206.sql
@@ -0,0 +1,4 @@
+INSERT INTO ensemble.dbo.roles (name) VALUES
+	 (N'Admin'),
+	 (N'Project Manager'),
+	 (N'Team Member');
